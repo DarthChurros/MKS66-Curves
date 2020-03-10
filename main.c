c853a3b5b2a31710172a8c4e32e886a35058b1ce
DarthChurros <tnarayan00@stuy.edu>
2020-03-10 09:54:20 -0400

    updated parser

--- main.c
+++ main.c
@@ -25,12 +25,12 @@ int main(int argc, char **argv) {
   /* print_matrix(make_rotZ(M_PI/4)); */
   /* printf("\n"); */
 
-  if ( argc == 2 )
+  if ( argc > 1 )
     parse_file( argv[1], transform, edges, s );
   else
     parse_file( "stdin", transform, edges, s );
 
-  
+
   free_matrix( edges );
   free_matrix( transform );
-}  
+}
--- parser.c
+++ parser.c
@@ -82,11 +82,12 @@ void parse_file ( char * filename,
     line[strlen(line)-1]='\0';
     //printf(":%s:\n",line);
 
-    double xvals[3];
-    double yvals[3];
+    double xvals[4];
+    double yvals[4];
     double zvals[4];
     struct matrix *tmp;
     double theta;
+    double r;
     char axis;
 
     if ( strncmp(line, "line", strlen(line)) == 0 ) {
@@ -171,7 +172,24 @@ void parse_file ( char * filename,
     }//end save
 
     else if (strncmp(line, "bezier", strlen(line)) == 0 ) {
-      
+      fgets(line, sizeof(line), f);
+      sscanf(line, "%lf %lf %lf %lf %lf %lf %lf %lf", xvals, yvals, xvals+1, yvals+1, xvals+2, yvals+2, xvals+3, yvals+3);
+
+      add_curve(edges, xvals[0], yvals[0], xvals[1], yvals[1], xvals[2], yvals[2], xvals[3], yvals[3], 0.05, BEZIER);
+    }
+
+    else if ( strncmp(line, "hermite", strlen(line)) == 0 ) {
+      fgets(line, sizeof(line), f);
+      sscanf(line, "%lf %lf %lf %lf %lf %lf %lf %lf", xvals, yvals, xvals+1, yvals+1, xvals+2, yvals+2, xvals+3, yvals+3);
+
+      add_curve(edges, xvals[0], yvals[0], xvals[1], yvals[1], xvals[2], yvals[2], xvals[3], yvals[3], 0.05, HERMITE);
+    }
+
+    else if ( strncmp(line, "circle", strlen(line)) == 0 ) {
+      fgets(line, sizeof(line), f);
+      sscanf(line, "%lf %lf %lf %lf", xvals, yvals, zvals, &r);
+
+      add_circle(edges, *xvals, *yvals, *zvals, r, 0.05);
     }
   }
 }
